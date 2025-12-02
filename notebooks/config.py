78a04d38ae00bbefb88473ed80382be84b04d0d7
Shaquille <99684747+Shaquillepj@users.noreply.github.com>
2025-12-01 23:47:20 -0500

    Delete notebooks directory

--- notebooks/config.py
+++
@@ -1,4 +0,0 @@
-# To replicate the data pull, create a profile on NYC Open Data and generate an app token. Then fil in the below fields
-APP_TOKEN = "APP_TOKEN_GOES_HERE"
-USERNAME = "USERNAME_GOES_HERE"
-PASSWORD = "PASSWORD_GOES_HERE"
